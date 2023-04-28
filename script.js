f3ed1ad463c19a5a5a52be7522e4e1badcffbfd5
Ajay Kumar <102378038+ajaykumar2pp@users.noreply.github.com>
2023-04-28 09:28:38 +0530

    add Ajay index file

---
+++ script.js
@@ -0,0 +1,25 @@
+const copyBtn = document.getElementById("copy-btn");
+const copyText = document.getElementById("copy-text");
+
+copyBtn.addEventListener("click", () => {
+  const textarea = document.createElement("textarea");
+  textarea.value = copyText.textContent;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand("copy");
+  document.body.removeChild(textarea);
+  alert("Text copied to clipboard!");
+});
+
+// ***********************
+// const copyBtn = document.getElementById("copy-btn");
+// const copyInput = document.getElementById("copy-input");
+
+// copyBtn.addEventListener("click", () => {
+//   copyInput.style.display = "block";
+//   copyInput.select();
+//   document.execCommand("copy");
+//   copyInput.style.display = "none";
+//   alert("Text copied to clipboard!");
+// });
+
